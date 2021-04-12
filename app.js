579037f23343e9f4b770ea646e65c4e518206529
Ekow <ewusiessel@gmail.com>
2021-04-13 00:46:23 +0100

    changed function syntax to arrow functions

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
-btn.addEventListener("click", function () {
+btn.addEventListener("click", () => {
   // get random number between 0 - 3
   const randomNumber = getRandomNumber();
   console.log(randomNumber);
